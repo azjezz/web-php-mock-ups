8349bbf3014b642268e17e0517cfb29966c2486d
azjezz <azjezz@protonmail.com>
2019-02-05 00:50:23 +0100

    update header; add search input

--- templates/landing.php
+++ templates/landing.php
@@ -7,7 +7,6 @@
     <span class='text-bold'>PHP</span> is a popular general-purpose scripting language that is especially suited to web development. <br /> <br />
     Fast, flexible and pragmatic, <span class='text-bold'>PHP</span> powers everything from your blog to the most popular websites in the world.
 </p>
-
 <pre class="code php"><code class="bg-dark text-light">&lt?php declare(strict_types=1);
 
 function greet(string $name): void
@@ -16,5 +15,3 @@ function greet(string $name): void
 }
 
 greet('World'); // Hello, World !</code></pre>
-
-<a href="/getting-started" class="btn btn-link p-centered">Get Started</a>
--- templates/layout/header.php
+++ templates/layout/header.php
@@ -8,25 +8,28 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/atom-one-dark.min.css">
     <link rel="shortcut icon" type="image/png" href="/asset/img/logo.png"/>
     <style>
-        header.navbar, div.hero.bg-gray.main {
+        header.navbar {
+            padding: 0.8em 2em;
+        }
+        div.hero.bg-gray.main {
             padding: 2em;
         }
     </style>
 </head>
 <body>
-    <header class="navbar">
-        <section class="navbar-section">
-            <a href="#" class="btn btn-link">Downloads</a>
-            <a href="#" class="btn btn-link">Docs</a>
-            <a href="#" class="btn btn-link">Extensions</a>
-        </section>
-        <section class="navbar-center">
-            <a href="/"><img src="/asset/img/logo.png" class="navbar-brand text-dark text-bold text-large"
-                style="width: 60px" /></a>
-        </section>
-        <section class="navbar-section">
-            <a href="#" class="btn btn-link">Community</a>
-            <a href="#" class="btn btn-link">Wiki</a>
-            <a href="#" class="btn btn-link">Blog</a>
-        </section>
+    <header class="navbar bg-dark">
+    <section class="navbar-section">
+        <a href="/" class="navbar-brand mr-2" style="color: #fff;">PHP</a>
+        <a href="/getting-started" class="btn btn-link text-secondary">Getting Started</a>
+        <a href="/downloads" class="btn btn-link text-secondary">Downloads</a>
+        <a href="/docs" class="btn btn-link text-secondary">Docs</a>
+        <a href="/get-involved" class="btn btn-link text-secondary">Get Involved</a>
+        <a href="/community" class="btn btn-link text-secondary">Community</a>
+        <a href="/news" class="btn btn-link text-secondary">News</a>
+    </section>
+    <section class="navbar-section">
+        <div class="input-group input-inline">
+        <input class="form-input" type="text" placeholder="search">
+        </div>
+    </section>
     </header>

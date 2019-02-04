c29e6a8ebfe0b3b2c9238a5cf6d7dca403caa86a
azjezz <azjezz@protonmail.com>
2019-02-04 23:02:50 +0100

    okay, this looks better

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ see : https://externals.io/message/104088
 ```console
 $ git clone https://github.com/azjezz/web-php-mock-ups
 $ cd web-php-mock-ups
+$ composer install
 $ cd public
 $ php -S 127.0.0.1:8080
 ```
@@ -16,3 +17,7 @@ $ php -S 127.0.0.1:8080
 
 ### Getting Started
 ![getting-started](https://github.com/azjezz/web-php-mock-ups/blob/master/screenshots/getting-started.png)
+
+### Installation
+
+![installation](https://github.com/azjezz/web-php-mock-ups/blob/master/screenshots/installation.png)
--- templates/landing.php
+++ templates/landing.php
@@ -4,21 +4,17 @@
     <span class='text-bold'>PHP</span> Hypertext Preprocessor
 </h2>
 <p>
-PHP is a popular general-purpose scripting language that is especially suited to web development. <br />
-Fast, flexible and pragmatic, PHP powers everything from your blog to the most popular websites in the world.
+    <span class='text-bold'>PHP</span> is a popular general-purpose scripting language that is especially suited to web development. <br /> <br />
+    Fast, flexible and pragmatic, <span class='text-bold'>PHP</span> powers everything from your blog to the most popular websites in the world.
 </p>
-<pre class="code php">
-    <code class="bg-dark text-light">
-&lt?php declare(strict_types=1);
+
+<pre class="code php"><code class="bg-dark text-light">&lt?php declare(strict_types=1);
 
 function greet(string $name): void
 {
     echo 'Hello, ' . $name . ' !';
 }
 
-greet('World'); // Hello, World !
-    </code>
-</pre>
-<a href="/getting-started" class="btn btn-link btn-lg p-centered ">Get Started</a>
+greet('World'); // Hello, World !</code></pre>
 
-<!-- we can have "card"s here of latest blog posts ? -->
+<a href="/getting-started" class="btn btn-link p-centered">Get Started</a>
